2a66ef4d0f98cb21732eeb04af404112ad51e194
drowzy <simon.thornqvist@gmail.com>
2015-11-09 22:18:53 +0100

    init tests

--- test/index.js
+++ test/index.js
@@ -1,10 +1,39 @@
 'use strict';
 
-var assert = require('assert');
-var picoProxy = require('../lib');
+var expect = require('chai').expect,
+    nock = require('nock'),
+    http = require('http'),
+    picoProxy = require('../lib');
 
 describe('pico-proxy', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
+  var proxy;
+  before(function () {
+    proxy = picoProxy({ target: 'http://api.domain.com' }).listen(3000);
+  });
+
+  after(function () {
+    proxy.close();
+  });
+
+  describe('OPTION requests', function() {
+    before(function () {
+      this.scope = nock('http://api.domain.com')
+                   .log(console.log)
+                   .get('/')
+                   .reply(304);
+    });
+
+    after(function () {
+      nock.cleanAll();
+    });
+
+    it('should intercept OPTIONS requests per default', function (done) {
+      var req = http.request({ hostname: '127.0.0.1', port: 3000, method: 'OPTIONS' }, function (res) {
+        expect(res.statusCode).to.be.eq(200);
+        done();
+      });
+      req.end();
+    });
+
   });
 });
